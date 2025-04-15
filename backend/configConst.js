54ba4a245d8793c56abcf86f91098f67291581f4
Nelly5469 <nelly.delgado5469@alumnos.udg.mx>
2025-04-15 14:05:13 -0600

    refactor: agregar archivo configConst.js

---
+++ backend/configConst.js
@@ -0,0 +1,9 @@
+const port = 5000;
+const nameDirectoryRequests = 'httpRequests'; // nombre de la carpeta que contiene las peticiones de cada tabla
+const nameDirectoryDicom = 'dicomEstudios'; // nombre de la carpeta que contiene los archivos dicom
+
+module.exports = { 
+    port, 
+    nameDirectoryRequests, 
+    nameDirectoryDicom 
+};
