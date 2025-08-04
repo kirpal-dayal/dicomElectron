d24e0a8f963593fe5fcea9a82a1d985b27390d47
Nelly5469 <nelly.delgado5469@alumnos.udg.mx>
2025-08-03 18:15:17 -0600

    refactor: agregar contrasena de bd como variable de entorno

--- backend/connectionDb.js
+++ backend/connectionDb.js
@@ -1,12 +1,14 @@
 //import mysql from 'mysql2';
 const mysql = require('mysql2');
+require('dotenv').config(); // Cargar variables de entorno
+const DB_PASSW = process.env.DB_PASSW;
 
 // Configura la conexión a la base de datos MySQL local de prueba
 // OCULTAR VALORES DE CONEXION CUANDO CAMBIEMOS A LA BD REAL!!!!
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '1234',
+  password: DB_PASSW,
   database: 'fibrosis_v06'
 });  // .promise es clave para que funcione con async/await al momento de estar con los json
 
