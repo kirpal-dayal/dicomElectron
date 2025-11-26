3d5853c03f78884470803987f0c91e8c0040bb12
Nelly5469 <nelly.delgado5469@alumnos.udg.mx>
2025-11-25 19:58:06 -0600

    docs: cambiar dónde guardar los logs

--- logging/logger.js
+++ logging/logger.js
@@ -1,16 +1,19 @@
 // general logger of the app using winston, controls where logs are stored and their format
 
 const { createLogger, transports, format } = require("winston");
+const path = require('path');
+const logsPath = path.join(__dirname, '/logs');
 
+//generic logger configuration
 const logger = createLogger({
-  level: "info", // Nivel de log (puedes cambiarlo a "debug", "error", etc.)
+  level: "info", // "debug", "error", etc.
   format: format.combine(
     format.timestamp(),
-    format.json() // Formato JSON para logs estructurados
+    format.json()
   ),
   transports: [
-    new transports.File({ filename: "logs/error.log", level: "error" }), // Logs de errores
-    new transports.File({ filename: "logs/combined.log" }) // Todos los logs
+    new transports.File({ filename: `${logsPath}/error.log`, level: "error" }), // Logs de errores
+    new transports.File({ filename: `${logsPath}/combined.log` }) // Todos los logs
   ],
 });
 
